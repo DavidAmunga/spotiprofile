65ec4ac62ed1f0deea45e50a61e71bfa0f5da9a8
David Amunga <david.onyimbo@gmail.com>
2021-10-25 16:56:13 +0300

    Setting Up React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { accessToken, logout, getCurrentUsersProfile } from './spotify';
 import { catchErrors } from './utils';
 import './App.css';
@@ -25,19 +26,36 @@ function App() {
             Login to Spotify
           </a>
         ) : (
-          <>
-            <div>Logged In!</div>
-            <button onClick={logout}>Log out</button>
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <h1>{profile.followers.total} Followers</h1>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
-          </>
+          <Router>
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlist/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={()=>logout()}>Log out</button>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <h1>{profile.followers.total} Followers</h1>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar" />
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
